eaa8c96a1951c715b379ae3a6ee0c8db0155efbb
kaichi0michael <ihciak.0127@gmail.com>
2025-03-13 07:03:08 +0000

    db追加、models.rb

--- db/migrate/20250313070148_create_notifications.rb
+++ db/migrate/20250313070148_create_notifications.rb
@@ -1,4 +1,8 @@
 class CreateNotifications < ActiveRecord::Migration[6.1]
   def change
+    create_table :notification do |t|
+      t.integer :user_id
+      t.timestamps
+    end
   end
 end
--- models.rb
+++ models.rb
@@ -3,3 +3,37 @@ require 'bcrypt'
 Bundler.require
 
 ActiveRecord::Base.establish_connection
+
+class User < ActiveRecord::Base
+    has_secure_password
+    has_many :posts
+    has_many :comments
+    has_many :likes
+    has_many :follows
+    has_many :notification
+end
+
+class Post < ActiveRecord::Base
+    belongs_to :user
+    has_many :comments
+    has_many :likes
+end
+
+class Comment < ActiveRecord::Base
+    belongs_to :user
+    belongs_to :post
+end
+
+class Like < ActiveRecord::Base
+    belongs_to :user
+    belongs_to :post
+end
+
+class Schedule < ActiveRecord::Base
+    belongs_to :user
+end
+
+class Notification < ActiveRecord::Base
+    belongs_to :user
+end
+
